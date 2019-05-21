e4234cdc9f42aa581149004371047492cbbb012f
Thect <32976729+cheetosysst@users.noreply.github.com>
2019-05-21 20:30:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
-import os
+from pathlib import Path
 import urllib.request as ul
+import os
 # Variable
 
-
+home = str(Path.home())
 # Get list of packages
 lineList = [line.rstrip('\n') for line in open("package.txt")]
 
@@ -14,4 +15,8 @@ if "install" == sys.argv[1]:
 	for i in lineList:
 		temp = i.split(";")
 		if sys.argv[2] == temp[0]:
-			ul.urlretrieve(temp[4], "./example.txt")
+			if not os.path.isdir(str(Path.home())+"/package"):
+				os.mkdir(str(Path.home())+"/package", mode=0o777 )
+				print ("dir made")
+			print(str(Path.home())+"/package/"+temp[4].split("/")[len(temp)-1])
+			ul.urlretrieve(temp[4], str(Path.home())+"/package/"+temp[4].split("/")[len(temp)-1])

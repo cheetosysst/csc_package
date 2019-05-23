931ad3e2f178142a007c1a10f61c642e0818574a
Thect <32976729+cheetosysst@users.noreply.github.com>
2019-05-23 16:29:50 +0800

    Update main.py
    Multiple function updated,minor tweaks to make the code look better and look better

--- main.py
+++ main.py
@@ -4,26 +4,40 @@ import urllib.request as ul
 import os
 import subprocess as sp
 
-# if debugMode:
-# 	print(bcolors.Warning+"[]DEBUG"+bcolors.ENDC)
-
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
 
-debugMode = False
+debugMode= False
 home = str(Path.home())
-# Get list of packages
 lineList = [line.rstrip('\n') for line in open("package.txt")]
 
 if "-d" in sys.argv:
 	debugMode = True
+
+if "update" == sys.argv[1]:
+	print("Updating " + bcolors.WARNING + "package.txt" + bcolors.ENDC)
+	# download from server
+	print("Finish Downloading, type" + bcolors.WARNING + " \"csc upgradable\" " + bcolors.ENDC + "for updatable packages")
+	exit()
+
+if "show" == sys.argv[1]:
+	packageName = sys.argv[2]
+	for i in lineList:
+		if i.split(";")[0].lower() == packageName.lower():
+			name = i.split(";")[0]
+			version = i.split(";")[1]
+			auther = i.split(";")[2]
+			description = i.split(";")[3]
+			print(bcolors.WARNING+bcolors.BOLD+name+bcolors.ENDC,"	",bcolors.HEADER+version+bcolors.ENDC,"	",bcolors.OKBLUE+auther+bcolors.ENDC)
+			print(description)
+	exit()
 
 if "search" == sys.argv[1]:
 	for i in lineList:
@@ -36,6 +50,7 @@ if "search" == sys.argv[1]:
 			description = i.split(";")[3]
 			print(bcolors.WARNING+bcolors.BOLD+name+bcolors.ENDC,"	",bcolors.HEADER+version+bcolors.ENDC,"	",bcolors.OKBLUE+auther+bcolors.ENDC)
 			print(description)
+	exit()
 
 if "list" == sys.argv[1]:
 	for i in lineList:
@@ -47,6 +62,7 @@ if "list" == sys.argv[1]:
 		description = i.split(";")[3]
 		print(bcolors.WARNING+bcolors.BOLD+name+bcolors.ENDC,"	",bcolors.HEADER+version+bcolors.ENDC,"	",bcolors.OKBLUE+auther+bcolors.ENDC)
 		print(description)
+	exit()
 
 if "install" == sys.argv[1]:
 	for i in lineList:
@@ -63,5 +79,12 @@ if "install" == sys.argv[1]:
 			ul.urlretrieve(temp[4], str(Path.home())+"/package/"+temp[4].split("/")[index])
 			print("Extracting package ...")
 			if debugMode:
-				print(["tar" ,"-xvf" ,str(Path.home())+"/package/"+temp[4].split("/")[index] ,str(Path.home())+"/package"])
-			sp.run(["tar" ,"-xvf" ,str(Path.home())+"/package/"+temp[4].split("/")[index] ,"--directory="+str(Path.home())+"/package"])
+				print(["tar" ,"-xf" ,str(Path.home())+"/package/"+temp[4].split("/")[index] ,str(Path.home())+"/package"])
+			sp.run(["tar" ,"-xf" ,str(Path.home())+"/package/"+temp[4].split("/")[index] ,"--directory="+str(Path.home())+"/package"])
+			print("Removing tar file")
+			os.remove(str(Path.home())+"/package/"+temp[4].split("/")[index])
+			print("Checking if make  config exist")
+			if temp[5] == "none":
+				print ("Config file not found, proceed to default install")
+			else:
+				print("Config file found at " ,bcolors.WARNING + temp[5]+bcolors.ENDC,", downloading ...")
